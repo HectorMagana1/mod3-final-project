606111f805419f3fcb0a33aed9e02311153bf164
Hector Magana <hectormagana8423@gmail.com>
2023-08-29 21:37:53 -0600

    worked on front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Index.css'
+import { Routes,Route,Navigate } from 'react-router-dom'
+
 import Navbar from './components/Navbar'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import LandingPage from './pages/LandingPage'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+
 
 export default function App() {
+
+  const [loggedIn, setLoggedIn] = useState(true)
+
   return (
-    <>
-      <Navbar />
-      <h1 className="text-3xl font-universal-font underline">
-        Hello world!
-      </h1>
-    </>
+    <div>
+      <Navbar loggedIn={loggedIn} />
+
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/' element={<LandingPage />} />
+        {loggedIn?
+          <>
+            <Route path='/dashboard' element={<Dashboard />} /> 
+            <Route path='/profile' element={<Profile />} /> 
+            {/* will need to add check for loaded data  */}
+            <Route path='*' element={<Navigate to='/dashboard' />} />
+          </> :
+          <>
+            <Route path='/login' element={<Login />} />
+            {/* will need to add check for loaded data  */}
+            <Route path='*' element={<Navigate to='/login' />} />
+          </>
+        }
+      </Routes>
+    </div>
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Navbar() {
-
-    let signedIn = true;
+export default function Navbar({ loggedIn }) {
 
   return (
     <>
-        <div>
-            <p>home</p>
-            {signedIn? <p>profile</p> : <p>log in</p>}
-            <p>contact</p>
+        <div className='w: bg-gray-400 flex justify-evenly items-center text-2xl h-16 font-universal-font'>
+            <Link to='/'>Home</Link>
+            {loggedIn? <Link to='/profile'>Profile</Link> : <Link to='/login'>Log In</Link>}
+            <Link to='/about'>About</Link>
         </div>
     </>
   )
