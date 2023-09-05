276f02be8492d100e06fb0396794bb6bb7cddd9f
Hector Magana <hectormagana8423@gmail.com>
2023-09-05 11:28:00 -0600

    almost done

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,13 +1,9 @@
 import React, { useState,useRef, useEffect } from 'react'
-import { DragDropContext } from 'react-beautiful-dnd'
-import DayOfWeek from '../components/ListOfWorkouts'
-import ListOfWorkouts from '../components/ListOfWorkouts'
 import axios from '../api'
 import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
 
-  const [input,setInput] = useState('')
   const [openForm,setOpenForm] = useState(false)
   const [exercises,setExercises] = useState([])
   const exerciseName = useRef()
@@ -27,16 +23,12 @@ export default function Dashboard() {
           Authorization:`Bearer ${localStorage.getItem('token')}`
         }
       })
-      exercises.push(newCreatedExercise.data)
+      exercises.unshift(newCreatedExercise.data)
     }
     catch(error){
       console.log(error)
     }
     setOpenForm((prev) => prev = !prev)
-  }
-
-  function handleChange(event){
-    setInput(event.target.value)
   }
 
   async function getExercises(){
@@ -52,25 +44,43 @@ export default function Dashboard() {
       console.log(error)
     }
   }
-  console.log(exercises)
+  // console.log(exercises)
 
   useEffect(() => {
     getExercises()
   },[])
+
+  async function handleDelete(exercise){
+    try{
+      await axios.delete(`/api/exercises/${exercise._id}`, {
+        headers: {
+          Authorization:`Bearer ${localStorage.getItem('token')}`
+        }
+      })
+      const newExercisesArr = exercises.filter((exerciseElement) => exerciseElement._id!==exercise._id)
+      setExercises(newExercisesArr)
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
 
   return (
       <div>
         <button onClick={open}>+</button>
         {openForm && 
         <form onSubmit={handleSubmit}>
-          <input onChange={handleChange} ref={exerciseName} type='text' placeholder='Create Workout' name='exerciseName' className='border-b-[1px] border-gray-400' />
+          <input ref={exerciseName} type='text' placeholder='Create Workout' name='exerciseName' className='border-b-[1px] border-gray-400' />
           <button className='shadow-lg px-2 py-[1px] rounded-xl hover:bg-gray-400 bg-gray-200'>Add</button>
         </form>
         }
         {exercises ? exercises.map((exercise) => {
           return(
-            <div key={exercise._id}>
+            <div className="w-40 flex justify-between" key={exercise._id}>
               <Link to={`/exercises/${exercise._id}`}>{exercise.exerciseName}</Link>
+              <div className='w-16 flex justify-between'>
+                <button onClick={() => handleDelete(exercise)}>X</button>
+              </div>
             </div>
           )
         }):
--- client/src/pages/Show.jsx
+++ client/src/pages/Show.jsx
@@ -1,11 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from '../api'
 
 export default function Show() {
 
     const {id} = useParams()
+    const [form,setForm] = useState(false)
     const [exercise,setExercise] = useState({})
+    const [loading,setLoading] = useState(false)
+    // const [setData,setSetData] = useState([])
+    const repsRef = useRef()
+    const weightRef = useRef()
+
+    function openForm(){
+        setForm((prev) => prev = !prev)
+    }
 
     async function getExercise(){
         try{
@@ -15,6 +24,8 @@ export default function Show() {
                 }
               })
             setExercise(showExercise.data)
+            // setSetData(showExercise.data.sets)
+            setLoading(true)
         }
         catch(error){
             console.log(error)
@@ -25,9 +36,56 @@ export default function Show() {
         getExercise()
     },[])
 
+    async function handleSubmit(event){
+        event.preventDefault()
+        try{
+            const newSetObj = {
+                reps:repsRef.current.value,
+                weight:weightRef.current.value
+            }
+            const newSet = await axios.post(`/api/sets/${id}`, newSetObj,{
+                headers: {
+                    Authorization:`Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            let sets = exercise.sets
+            sets.push(newSet.data)
+            setExercise({...exercise, sets})
+            // console.log(newSet.data);
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+    // console.log(setData)
+    // console.log(exercise)
   return (
     <div>
         <h1>{exercise.exerciseName}</h1>
+        <button onClick={openForm}>+</button>
+
+        {form && 
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="reps" placeholder="Reps" ref={repsRef} />
+                <input type="text" name="weight" placeholder="Weight" ref={weightRef} />
+                <button>add</button>
+            </form> 
+        }
+
+        {loading && exercise.sets.length>0 && 
+        <div>
+            {exercise.sets.map((set) => {
+                return(
+                    <div>
+                        <h1>{set.reps}</h1>
+                        <h1>{set.weight}</h1>
+                    </div>
+                )
+                })
+            }
+        </div>
+        }
+
     </div>
   )
 }
