48138c8d48e088c0c34d0ef077b04883ff17ab33
Hector Magana <hectormagana8423@gmail.com>
2023-09-06 13:14:02 -0600

    trying to connect deployed front end

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://mod3-backend-33gx.onrender.com',
+      '/api':'https://mod3-backend-33gx.onrender.com/api',
       '/auth':'https://mod3-backend-33gx.onrender.com'
     }
   },
