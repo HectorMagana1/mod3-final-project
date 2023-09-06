91f804b025cf321327df50bd810a57e60cfed64c
Hector Magana <hectormagana8423@gmail.com>
2023-09-06 13:22:09 -0600

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://mod3-backend-33gx.onrender.com/api',
+      '/api':'https://mod3-backend-33gx.onrender.com',
       '/auth':'https://mod3-backend-33gx.onrender.com'
     }
   },
